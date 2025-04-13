395bd1b1114688e42352d2ef4e899f7908fe861c
Ahmet Kürşad Şaşmaz <ahmetksasmaz@unknown56bdf4829c19.home>
2025-04-13 23:54:10 +0300

    Experimentations are done for phase_2

---
+++ covert_test.sh
@@ -0,0 +1,103 @@
+EXPERIMENT_FILE=$1
+SCENARIO_NUMBER=$2
+REPEAT=$3
+DROP_RATE=$4
+K_BITS=$5
+CONSISTENT_COUNT=0
+
+rm -rf $EXPERIMENT_FILE
+
+# Restart mitm
+docker compose exec python-processor sh -c "pkill -9 python3"
+docker compose exec -T python-processor sh -c "python3 /code/python-processor/dropper.py $DROP_RATE &"
+sleep 0.5
+
+send_0_bit() {
+    docker compose exec sec sh -c "ping insec -0 -i 0.2" | tail -n 3 >> $EXPERIMENT_FILE
+}
+
+send_1_bit() {
+    docker compose exec sec sh -c "ping insec -1 -i 0.2" | tail -n 3 >> $EXPERIMENT_FILE
+}
+
+send_eof(){
+    docker compose exec sec sh -c "ping insec -2 -i 0.2" | tail -n 3 >> $EXPERIMENT_FILE
+
+    while [ -z "$(tail -n 1 $EXPERIMENT_FILE)" ]; do
+        docker compose exec sec sh -c "ping insec -2 -i 0.2" | tail -n 3 >> $EXPERIMENT_FILE
+    done
+}
+
+start_insec(){
+    docker compose exec insec sh -c "rm -rf /code/insec/10.1.0.21.txt"
+    docker compose exec -T insec sh -c "tcpdump -i eth0 icmp and icmp[icmptype]=icmp-echo -n -l | python3 /code/insec/dump_parser.py -f &"
+    sleep 0.5
+}
+
+stop_insec(){
+    docker compose exec insec sh -c "pkill -9 tcpdump"
+}
+
+cmp_insec_result(){
+    if [ "$1" == "$(docker compose exec insec sh -c "cat /code/insec/10.1.0.21.txt")" ]; then
+        CONSISTENT_COUNT=$((CONSISTENT_COUNT + 1))
+    fi
+}
+
+# Scenario Number 1 : Sending bit-0 N times
+
+send_0_n_times() {
+    for i in $(seq 1 $REPEAT); do
+        start_insec
+        send_0_bit
+        send_eof
+        stop_insec
+        cmp_insec_result "0"
+    done
+
+    echo "Consistent Rate : [$CONSISTENT_COUNT / $REPEAT]" >> $EXPERIMENT_FILE
+}
+
+# Scenario Number 2 : Sending bit-1 N times
+send_1_n_times() {
+    for i in $(seq 1 $REPEAT); do
+        start_insec
+        send_1_bit
+        send_eof
+        stop_insec
+        cmp_insec_result "1"
+    done
+
+    echo "Consistent Rate : [$CONSISTENT_COUNT / $REPEAT]" >> $EXPERIMENT_FILE
+}
+
+# Scenario Number 3 : Sending k bits N times
+send_k_n_times() {
+    for i in $(seq 1 $REPEAT); do
+        RANDOM_BINARY_STRING=$(head /dev/urandom | LC_ALL=C tr -dc '01' | head -c $K_BITS)
+
+        start_insec
+        
+        for (( j=0; j<$K_BITS; j++ )); do
+            if [ "${RANDOM_BINARY_STRING:$j:1}" == "0" ]; then
+                send_0_bit
+            else
+                send_1_bit
+            fi
+        done
+
+        send_eof
+        stop_insec
+        cmp_insec_result $RANDOM_BINARY_STRING
+    done
+
+    echo "Consistent Rate : [$CONSISTENT_COUNT / $REPEAT]" >> $EXPERIMENT_FILE
+}
+
+if [ "$SCENARIO_NUMBER" == "1" ]; then
+    send_0_n_times
+elif [ "$SCENARIO_NUMBER" == "2" ]; then
+    send_1_n_times
+elif [ "$SCENARIO_NUMBER" == "3" ]; then
+    send_k_n_times
+fi
---
+++ results/phase_2/covert_results/parser_3_4.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+
+file = open('exp3_4_working.txt', 'r')
+
+lines = file.readlines()
+file.close()
+
+sum_tx = []
+sum_rx = []
+
+sum_tx.append(0)
+sum_rx.append(0)
+
+for line in lines:
+    if line == '\n':
+        sum_tx.append(0)
+        sum_rx.append(0)
+        continue
+    else:
+        left = line.split(",")[0]
+        right = line.split(",")[1]
+
+        left = int(left)
+        right = int(right)
+        sum_tx[-1] += left
+        sum_rx[-1] += right
+
+sumsum_tx = sum(sum_tx)
+sumsum_rx = sum(sum_rx)
+
+mean_tx = sum(sum_tx) / len(sum_tx)
+mean_rx = sum(sum_rx) / len(sum_rx)
+
+std_tx = 0
+std_rx = 0
+
+for i in range(len(sum_tx)):
+    std_tx += (sum_tx[i] - mean_tx) ** 2
+    std_rx += (sum_rx[i] - mean_rx) ** 2
+std_tx = (std_tx / len(sum_tx)) ** 0.5
+std_rx = (std_rx / len(sum_rx)) ** 0.5
+
+conf_tx = 1.96 * (std_tx / (len(sum_tx) ** 0.5))
+conf_rx = 1.96 * (std_rx / (len(sum_rx) ** 0.5))
+
+print("Total tx: ", sumsum_tx)
+print("Total rx: ", sumsum_rx)
+print("Mean tx: ", mean_tx)
+print("Mean rx: ", mean_rx)
+print("Std tx: ", std_tx)
+print("Std rx: ", std_rx)
+print("Conf tx: ", conf_tx)
+print("Conf rx: ", conf_rx)
+print("Mean - conf tx: ", mean_tx - conf_tx)
+print("Mean + conf tx: ", mean_tx + conf_tx)
